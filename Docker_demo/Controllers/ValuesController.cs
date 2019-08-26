45fbb90e63f578a6064babf4e69f44b3e567c090
Jovan Pesic <enimen@elfak.rs>
2019-08-26 20:47:40 +0200

    Add project files.

---
+++ Docker_demo/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceStack.Redis;
+
+namespace Docker_demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private RedisManagerPool _redis;
+        public ValuesController(IServiceProvider service)
+        {
+            _redis = service.GetService<RedisManagerPool>();
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            int inc = 0;
+            if(int.TryParse(_redis.GetClient().GetValue("inc"),out inc))
+            {
+                inc++;
+            }
+            _redis.GetClient().SetValue("inc", inc.ToString());
+            return new string[] { "Site has been visited:" + inc.ToString() };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
